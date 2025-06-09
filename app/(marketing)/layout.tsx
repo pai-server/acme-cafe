ed905305b1fda82eaf9b05e1db27fda21bd85eb3
Alex980102 <74475326+Alex980102@users.noreply.github.com>
2025-06-09 14:55:32 -0600

    feat: add floating marketing banner component with expand/collapse functionality

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header';
+import { MarketingBanner } from '@/components/marketing-banner';
 
 export default function MarketingLayout({
   children,
@@ -9,6 +10,7 @@ export default function MarketingLayout({
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">{children}</main>
+      <MarketingBanner />
     </div>
   );
 } 
